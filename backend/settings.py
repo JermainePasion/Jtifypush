b83a55dcf4cb35299c805914af59669b317026e4
Jermaine_Riley Pasion <jmpasion@student.hau.edu.ph>
2024-04-18 14:55:58 +0800

    Allowed host

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-1c$e&2#ari&@+z@a1k9gbs@hoyw+rwi_#8&vv=ax-sr%ezk+ax
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
